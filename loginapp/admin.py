e6be7fb2aaceb947835475d44c4463f8580dcf67
Alexander Nuñez <alerxp@gmail.com>
2025-04-24 14:53:03 -0300

    Personalización: mejoras al panel y cambio de títulos

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import CUEUsuario
 
 admin.site.register(CUEUsuario)
+admin.site.site_header = "Sistema Provincial"
+admin.site.site_title = "Login Provincial"
+admin.site.index_title = "Administración"
