4c57e4f1a6b5678cfa5eab0f8bb517bf7619c8e0
Alexander Nuñez <alerxp@gmail.com>
2025-05-29 17:14:19 -0300

    Actualización del panel admin

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import CUEUsuario
 
-admin.site.register(CUEUsuario)
+@admin.register(CUEUsuario)
+class CUEUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cue', 'anexo', 'modalidad', 'nivel')
+    list_filter = ('cue', 'anexo', 'modalidad', 'nivel')
+    search_fields = ('user__username', 'cue')
+
+# Personalización del panel admin
 admin.site.site_header = "Sistema Provincial"
 admin.site.site_title = "Login Provincial"
 admin.site.index_title = "Administración"
